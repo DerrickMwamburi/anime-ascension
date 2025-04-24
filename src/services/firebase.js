aa83f3512e0de98fafef90da6d17955e129af30e
Derrick Katute <derrick.katute@student.moringaschool.com>
2025-04-24 10:37:34 +0300

    add cart functionality

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBfbH4yfzcvHLQ57dgIDYboFeeFdzbELOM",
+    authDomain: "anime-ascension.firebaseapp.com",
+    projectId: "anime-ascension",
+    storageBucket: "anime-ascension.firebasestorage.app",
+    messagingSenderId: "853913987245",
+    appId: "1:853913987245:web:bf1cd07d71507443923e24",
+    measurementId: "G-W3TMT1N70B"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
